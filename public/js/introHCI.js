117b2a248bec114aafaf337f57f84a5bcffe426e
Nan <itisnanful@gmail.com>
2014-02-27 19:16:11 +0000

    Added Experiment Header

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -9,5 +9,9 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
+  $('.likeBtn').click(function() {
+    ga('send', 'event', 'like', 'click');
+  });
+
 	// your code here
 }
--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,18 @@ var projects = require('../projects.json');
  */
 
 exports.view = function(req, res){
+    //var data = {'alt': false};
+    projects['alt'] = false;
   	res.render('index', projects);
 };
+exports.viewGrid = function(req, res) {
+    projects['alt'] = true;
+    //var data = {'alt': true};
+    res.render('index', projects);
+};
+
+exports.viewAlternate = function(req, res) {
+   var data = {'showAlternate': true};
+    res.render('index', projects);
+};
+
